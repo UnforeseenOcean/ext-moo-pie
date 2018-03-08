61b8cf74f59b7b741c6ae9d16c1a8ba78c97e693
rektide <rektide@voodoowarez.com>
2018-03-08 11:05:33 -0500

    Rework codebase to handle mootools recursive flatten calling properly.

--- ext-moo-pie.js
+++ ext-moo-pie.js
@@ -6,12 +6,13 @@
 // // ==/UserScript==
 
 // via https://github.com/mootools/mootools-core/blob/master/Source/Types/Array.js
-Array.prototype.flatten = function(){
+function flatten(){
 	var array = [];
 	for (var i = 0, l = this.length; i < l; i++){
 		var type = typeOf(this[i]);
 		if (type == 'null') continue;
-		array = array.concat((type == 'array' || type == 'collection' || type == 'arguments' || instanceOf(this[i], Array)) ? Array.flatten(this[i]) : this[i]);
+		array = array.concat((type == 'array' || type == 'collection' || type == 'arguments' || instanceOf(this[i], Array)) ? flatten(this[i]) : this[i]);
 	}
 	return array;
 }
+Array.prototype.flatten = flatten
